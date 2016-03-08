db8e4b67574ca2d05ed09255a61d8574e4d6919f
Will Cook <wellsee83@gmail.com>
2016-03-07 19:08:37 -0600

    Adventurer Mode

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,18 @@
 var length = prompt('What is the length?');
+while (isNaN(length)){
+  	alert('Please enter a numeric value!');
+  	length = prompt('What is the length?');
+}
 var width = prompt('What is the width?');
+while (isNaN(width)){
+  	alert('Please enter a numeric value!');
+  	width = prompt('What is the width?');
+}
 var sqFeet = (length*width);
-console.log("How many square feet do you need to paint? " + sqFeet);
 var gallons = Math.ceil(sqFeet/350);
+
+
+
+
+console.log("How many square feet do you need to paint? " + sqFeet);
 console.log("You will need to purchase " + gallons + " gallons of paint to cover " + sqFeet + ".");
